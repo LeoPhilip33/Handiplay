eb01108f7b8e177856e74b6fb913abe4472f73a0
Léo <leo.philip33.bordeaux@gmail.com>
2020-04-18 17:33:35 +0100

    menuv1

---
+++ Assets/ButtonOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonOptions : MonoBehaviour
+{
+    public GameObject hideOptions;
+    public GameObject hideJouer;
+    public GameObject hideCredits;
+    public GameObject OptionsMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClick()
+    {
+        hideOptions.SetActive(false);
+        hideJouer.SetActive(false);
+        hideCredits.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+}
